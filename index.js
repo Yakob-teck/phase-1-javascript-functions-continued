4e230d44d852b7a70fee42408769b2e493f6ca1e
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-04-08 08:55:40 -0600

    function continued

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+
+function saturdayFun(name="roller-skate"){
+  
+    return `This Saturday, I want to ${name}!`;
+    
+}
+ const mondayWork=function(duity='go to the office'){
+return `This Monday, I will ${duity}.`;
+}
+
+
+function wrapAdjective(highlight="*"){
+    return function (adjective="hard worker")
+    {
+        return `You are ${highlight}${adjective}${highlight}!`;
+    }
+    
+}
